d58977b5bd22a1496c89faa70062a04e02362378
rpanca15 <panca151204@gmail.com>
2024-11-11 21:15:27 +0700

    ui: index app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,9 +16,9 @@
         <div class="flex gap-8 justify-between items-center mx-auto">
             <img src="{{ asset('assets/images/logo.png') }}" alt="Buket_ku.id Logo" class="w-[175px] h-[auto]">
 
-            <ul class="flex gap-6 items-center text-base">
+            <ul class="flex gap-6 items-center justify-between text-base">
                 <li><a href="#" class="hover:text-violet-700">Home</a></li>
-                <div class="relative inline-block text-left group px-1">
+                <li class="relative inline-block text-left group px-1">
                     <button class="px-3 py-2 hover:text-violet-700">
                         Catalog
                     </button>
@@ -28,23 +28,19 @@
                         <div class="py-1">
                             <a href="#"
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-violet-700">
-                                Kategori 1
+                                Artificial
                             </a>
                             <a href="#"
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-violet-700">
-                                Kategori 2
+                                Graduation
                             </a>
                             <a href="#"
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-violet-700">
-                                Kategori 3
-                            </a>
-                            <a href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-violet-700">
-                                Kategori 4
+                                Snack
                             </a>
                         </div>
                     </div>
-                </div>
+                </li>
                 <li><a href="#" class="hover:text-violet-700">Order</a></li>
                 <li><a href="{{ route('admin') }}" class="hover:text-violet-700">Dashboard</a></li>
             </ul>
@@ -57,41 +53,42 @@
             </form>
 
             @guest
-                <div class="flex gap-4 items-center justify-center">
+                <div class="flex gap-6 items-center justify-center">
                     <a href="{{ route('login') }}" title="Login"
-                        class="text-violet-700 rounded hover:text-violet-800 transition ease-in-out duration-300">
+                        class="text-violet-900 rounded hover:text-violet-700 transition ease-in-out duration-300">
                         <i class="fas fa-sign-in text-[20px] font-bold"></i>
                     </a>
                     <a href="{{ route('register') }}" title="Register"
-                        class="text-violet-700 rounded hover:text-violet-800 transition ease-in-out duration-300">
+                        class="text-violet-900 rounded hover:text-violet-700 transition ease-in-out duration-300">
                         <i class="fas fa-user-plus text-[20px] font-bold"></i>
                     </a>
                 </div>
             @endguest
 
             @auth
-                <div class="flex gap-4 items-center jusitfy-center">
+                <div class="flex gap-6 items-center jusitfy-center">
                     <a href="{{ route('admin') }}" title="Cart"
-                        class="text-violet-800 rounded hover:text-violet-700 transition ease-in-out duration-300">
+                        class="text-violet-900 rounded hover:text-violet-700 transition ease-in-out duration-300">
                         <i class="fas fa-cart-shopping text-[20px]"></i>
                     </a>
                     @if (Auth::user()->role == 'admin')
                         <a href="{{ route('admin') }}" title="Dashboard"
-                            class="text-violet-800 rounded hover:text-violet-700 transition ease-in-out duration-300">
+                            class="text-violet-900 rounded hover:text-violet-700 transition ease-in-out duration-300">
                             <i class="fas fa-dashboard text-[20px]"></i>
                         </a>
                     @endif
                     <a href="{{ route('admin') }}" title="Profile"
-                        class="text-violet-800 rounded hover:text-violet-700 transition ease-in-out duration-300">
+                        class="text-violet-900 rounded hover:text-violet-700 transition ease-in-out duration-300">
                         <i class="fas fa-circle-user text-[20px]"></i>
                     </a>
                 </div>
                 <form action="{{ route('logout') }}" method="POST" class="inline">
                     @csrf
                     <button type="submit" title="Logout"
-                        class="text-red-500 rounded-lg hover:text-red-600 transition ease-in-out duration-300 relative group">
+                        class="text-red-700 rounded-lg hover:text-red-400 transition ease-in-out duration-300 relative group">
                         <i class="fas fa-sign-out-alt text-2xl flex opacity-100 group-hover:opacity-0"></i>
-                        <i class="fas fa-person-running text-2xl absolute left-0 top-0 opacity-0 group-hover:opacity-100  transition ease-in-out duration-300"></i>
+                        <i
+                            class="fas fa-person-running text-2xl absolute left-0 top-0 opacity-0 group-hover:opacity-100  transition ease-in-out duration-300"></i>
                     </button>
                 </form>
             @endauth
@@ -143,20 +140,23 @@
                 </div>
                 <div class="flex flex-col ml-5 w-[55%] max-md:ml-0 max-md:w-full">
                     <div
-                        class="flex flex-col grow mt-9 text-3xl font-bold leading-tight text-violet-900 max-md:mt-10 max-md:max-w-full">
-                        <h2 class="self-start ml-7 max-md:ml-2.5">Our Location</h2>
-                        <img loading="lazy"
-                            src="https://cdn.builder.io/api/v1/image/assets/8d90719630234040b8c6e31cc1469133/1129c489c35e5c403b6a72a63e48bc99b1f3ee37047eed9ac7592c022dbef70c?apiKey=8d90719630234040b8c6e31cc1469133&"
-                            alt="Map showing our location"
-                            class="object-contain mt-2 w-full aspect-[1.77] max-md:max-w-full" />
+                        class="flex flex-col gap-4 grow mt-9 text-3xl font-bold leading-tight text-violet-900 max-md:mt-10 max-md:max-w-full">
+                        <h2 class="self-start">Our Location</h2>
+                        <div class="relative w-full h-full flex-grow">
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1761.8664687535904!2d110.84770795550463!3d-7.546987133769291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a17476dfb651b%3A0xc9260aaad1c657e8!2sHomemade%20buket%20snack%20(Pre-order)!5e0!3m2!1sid!2sid!4v1731332756561!5m2!1sid!2sid"
+                                style="border:0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
+                                loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
         <hr
             class="shrink-0 mt-20 max-w-full h-px border border-solid border-black border-opacity-10 w-[1240px] max-md:mt-10" />
         <p class="mt-7 ml-6 text-sm text-right text-black text-opacity-60">
-            Shop.co © 2000-2023, All Rights Reserved
+            Buket_ku.co © 2000-{{ now()->year }}, All Rights Reserved
         </p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
